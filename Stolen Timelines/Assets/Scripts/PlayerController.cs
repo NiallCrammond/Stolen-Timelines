42ee74ca43cc8891fd87d0a5e82d287a72e77c94
NiallCrammond <2102699@uad.ac.uk>
2024-02-06 12:38:38 +0000

    WallJump
    Wall jump and sprite flip implemented, variables (forces, speed need tweaked to fit game feel)

--- Stolen Timelines/Assets/Scripts/PlayerController.cs
+++ Stolen Timelines/Assets/Scripts/PlayerController.cs
@@ -5,30 +5,49 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    private CustomInput input = null;
-    private Vector2 moveVec = Vector2.zero;
+    // Input variables
+    private CustomInput input = null; //Input system declaration
+    private Vector2 moveVec = Vector2.zero; // Read movement input
+    private float jumpInput = 0; // read jump inpuy
    
+    //Jump logic
     [SerializeField]
-    private float jumpForce;
-    private float jumpInput = 0;
+    private float jumpForce; 
     private bool jumpPressed = false;
     public LayerMask groundLayer;
     private bool isGrounded;
     RaycastHit2D groundHit;
 
+    //Transforms for grounded/ceiling check
     [SerializeField]
     private Transform groundCheck;
     [SerializeField]
     private Transform ceilingCheck;
+    [SerializeField]
+    private Transform wallCheck;
+    
 
+    //Player Movement variables
     [SerializeField]
     private float speed= 5;
-    private PlayerMovement playerMovement;
-    private bool movePressed = false;
+    [SerializeField]
+    private float maxSpeed = 10;
+    private PlayerMovement playerMovement; // refernence to player movement script
+    private bool movePressed = false; // check for input
+
+    //Wall Jump Variables
+    [SerializeField]
+    private Vector2 wallJumpForce = Vector2.zero;
+    RaycastHit2D wallHit;
+    [SerializeField]
+    LayerMask wallLayer;
+    [SerializeField]
+    private bool isWalled;
+   
 
     private void Awake()
     {
-        input = new CustomInput();
+        input = new CustomInput(); 
         playerMovement = GetComponent<PlayerMovement>();
     }
 
@@ -36,13 +55,11 @@ public class PlayerController : MonoBehaviour
     {
         input.Enable();
         input.Player.Movement.performed += OnMovePerformed;
-        input.Player.Movement.canceled += OnMoveCancelled;
+        input.Player.Movement.canceled += OnMoveCanceled;
       
         input.Player.Jump.performed += OnJumpPerformed;
-        input.Player.Jump.canceled += OnJumpPerformed;
-
-        
-     
+        input.Player.Jump.canceled += OnJumpCanceled;
+   
 
     }
 
@@ -50,40 +67,69 @@ public class PlayerController : MonoBehaviour
     {
         input.Disable();
         input.Player.Movement.performed -= OnMovePerformed;
-        input.Player.Movement.canceled -= OnMoveCancelled;
+        input.Player.Movement.canceled -= OnMoveCanceled;
 
 
         input.Player.Jump.performed -= OnJumpPerformed;
-        input.Player.Jump.canceled -= OnJumpPerformed;
+        input.Player.Jump.canceled -= OnJumpCanceled;
     }
 
     private void FixedUpdate()
     {
-        
- 
+
+        playerMovement.flip(transform);
         if (movePressed)
         {
-            playerMovement.move(moveVec, speed);
-            
+            playerMovement.move(moveVec, speed, maxSpeed);
+            Debug.Log("Moving");
         }
 
         else
         {
+           // Debug.Log("stationary");
+
             //Don't Move
             //Idle animation
         }
 
 
-        groundHit = Physics2D.Raycast(groundCheck.position, -Vector2.up, 0.1f, groundLayer);
+        groundHit = Physics2D.Raycast(groundCheck.position, -Vector2.up, 0.0f, groundLayer);
 
         if(groundHit.collider != null)
         {
-
-           if(jumpPressed)
+            isGrounded = true;
+           if(jumpPressed && isGrounded)
            {
             playerMovement.jump(jumpInput, jumpForce);
+                Debug.Log("Jump");
+
            }
 
+        }
+
+        else
+        {
+            isGrounded = false;
+        }
+
+        wallHit = Physics2D.Raycast(wallCheck.position, Vector2.right, 0.2f, wallLayer);
+
+        if (wallHit.collider != null)
+        {
+            isWalled = true;
+       
+            playerMovement.Walled();
+                
+            
+            if(jumpPressed && isWalled)
+            {
+                playerMovement.wallJump(wallJumpForce);
+
+            }
+        }
+        else
+        {
+            isWalled = false;
         }
 
     }
@@ -94,7 +140,7 @@ public class PlayerController : MonoBehaviour
         movePressed = true;
     }
 
-    private void OnMoveCancelled(InputAction.CallbackContext val)
+    private void OnMoveCanceled(InputAction.CallbackContext val)
     {
         moveVec = Vector2.zero;
         movePressed = false;
@@ -104,14 +150,26 @@ public class PlayerController : MonoBehaviour
     {
         jumpInput = val.ReadValue<float>();
         jumpPressed = true;
-        Debug.Log("Jumping");
+      //  Debug.Log("Jumping");
     }
 
 
-    private void OnJumpCancelled(InputAction.CallbackContext val)
+    private void OnJumpCanceled(InputAction.CallbackContext val)
     {
         jumpInput = val.ReadValue<float>();
         jumpPressed = false;
+    }
+
+    private void flip()
+    {
+        if(moveVec.x > 0)
+        {
+            transform.localScale = (new Vector3(1, 1, 1));
+        }
+        else if (moveVec.x < 0)
+        {
+            transform.localScale = (new Vector3(-1, 1, 1));
+        }
     }
 
 
--- Stolen Timelines/Assets/Scripts/PlayerMovement.cs
+++ Stolen Timelines/Assets/Scripts/PlayerMovement.cs
@@ -12,18 +12,38 @@ public class PlayerMovement : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
     }
-    public void move(Vector2 inputVec, float speed)
+    public void move(Vector2 inputVec, float speed, float maxSpeed)
     {
-        rb.velocity = new Vector2(Mathf.Clamp(rb.velocity.x, -50f, 50f), rb.velocityY);
-
-            rb.AddForceX(inputVec.x * speed);
-            Debug.Log("move attempted");
-        
+        rb.velocity = new Vector2(Mathf.Clamp(rb.velocity.x, -maxSpeed, maxSpeed), rb.velocityY);
+        rb.AddForceX(inputVec.x * speed);
+     
     }
 
     public void jump(float jumpInput, float jumpForce)
     {
         rb.AddForceY(jumpForce* jumpInput, ForceMode2D.Impulse);
       
+    }
+
+    public void wallJump(Vector2 wallJumpForce)
+    {
+        rb.AddForce(wallJumpForce, ForceMode2D.Impulse);
+    }
+
+    public void Walled()
+    {
+        rb.velocityY = 2.0f;
+    }
+
+    public void flip(Transform playerTransform)
+    {
+        if (rb.velocityX > 0)
+        {
+            transform.localScale = (new Vector3(1, playerTransform.localScale.y, transform.localScale.z));
+        }
+        else if (rb.velocityX < 0)
+        {
+            transform.localScale = (new Vector3(-1, playerTransform.localScale.y, transform.localScale.z));
+        }
     }
 }
