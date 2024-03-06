cafd5c3bd46dfaac615795e0da161128f4d3be91
2102500 <98824581+2102500@users.noreply.github.com>
2024-03-06 14:14:45 +0000

    implemented brazen bull

---
+++ Stolen Timelines/Assets/Scripts/BrazenBull.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrazenBull : MonoBehaviour
+{
+    public PlayerController player;
+    private BoxCollider2D col;
+    public int damageDealt;
+    public bool isReady;
+    // Start is called before the first frame update
+    void Start()
+    {
+        col = GetComponent<BoxCollider2D>();
+        col.isTrigger = true;
+        isReady = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isReady = true;
+            StartCoroutine(TakeDamage());
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        isReady = false;
+    }
+
+    public IEnumerator TakeDamage()
+    {
+        while (isReady)
+        {
+            yield return new WaitForSeconds(2.0f);
+            if (!isReady)
+            {
+                yield break;
+            }
+            else
+            {
+                player.health -= damageDealt;
+            }
+        }
+    }
+}
