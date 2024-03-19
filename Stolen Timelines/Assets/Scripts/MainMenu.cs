de9a6ef808dde07ab22859fd993592b09807b19d
MtotMtot <126000669+MtotMtot@users.noreply.github.com>
2024-03-19 13:14:21 +0000

    quota and score working
    quota and score reset from main menu to playing game.
    initial camera bounds setup...

--- Stolen Timelines/Assets/Scripts/MainMenu.cs
+++ Stolen Timelines/Assets/Scripts/MainMenu.cs
@@ -4,24 +4,27 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
-{
+{ 
+
+    [SerializeField]
+    private QuotaData quotaData;
+    [SerializeField]
+    private ScoreData scoreData;
+
     private LevelManager levelManager;
     private void Awake()
     {
-        levelManager = GameObject.FindWithTag("LevelManager").GetComponent<LevelManager>();
-
-        QuotaData quotaData = new QuotaData();
-        ScoreData scoreData = new ScoreData();              
+        levelManager = GameObject.FindWithTag("LevelManager").GetComponent<LevelManager>();              
         
     }
 
     public void playGame()
     {
-      //  StartCoroutine(levelManager.loadLevel(1));
-        
-        // SceneManager.LoadScene("BuildSubmissionV1");
-
-
+        quotaData.quotaRemain = 200;
+        quotaData.quotaLevel = 1;
+        quotaData.daysLeft = 3;
+        scoreData.score = 0;
+        scoreData.itemsCollected = 0;
     }
 
     public void quitGame()
