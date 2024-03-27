273fee5f6f46aa4d16dc3228b7e46d86d20d5815
2102500 <98824581+2102500@users.noreply.github.com>
2024-03-27 15:55:47 +0000

    added orange overlay when in brazen bulls colliders

--- Stolen Timelines/Assets/Scripts/BrazenBull.cs
+++ Stolen Timelines/Assets/Scripts/BrazenBull.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BrazenBull : MonoBehaviour
 {
     private PlayerController player;
+    private GameObject overlay;
     private BoxCollider2D col;
     public int damageDealt;
     public float bullCooldown;
@@ -15,23 +17,34 @@ public class BrazenBull : MonoBehaviour
     private void Awake()
     {
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+        overlay = GameObject.FindWithTag("DamageOverlay");
     }
     void Start()
     {
         col = GetComponent<BoxCollider2D>();
         col.isTrigger = true;
+        overlay .SetActive(false);
     }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
+            overlay.SetActive(true);
             timer += Time.deltaTime;
             if (timer > bullCooldown)
             {
                 player.health -= damageDealt;
                 timer = 0;
             }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            overlay.SetActive(false);
         }
     }
 
