35eea5bfd33603c8e0490a41d3df9740d0077cba
2102500 <2102500@uad.ac.uk>
2024-04-16 11:06:13 +0100

    re-added brazen bull changes

--- Stolen Timelines/Assets/Scripts/BrazenBull.cs
+++ Stolen Timelines/Assets/Scripts/BrazenBull.cs
@@ -1,32 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BrazenBull : MonoBehaviour
 {
     private PlayerController player;
+    //private GameObject overlay;
     private BoxCollider2D col;
     public int damageDealt;
-    public bool isReady;
-    // Start is called before the first frame update
+    public float bullCooldown;
+    public float timer;
+    //Start is called before the first frame update
 
     private void Awake()
     {
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+        //overlay = GameObject.FindWithTag("DamageOverlay");
     }
     void Start()
     {
         col = GetComponent<BoxCollider2D>();
         col.isTrigger = true;
-        isReady = false;
+        //overlay.SetActive(false);
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            isReady = true;
-            StartCoroutine(TakeDamage());
+            //overlay.SetActive(true);
+            timer += Time.deltaTime;
+            if (timer > bullCooldown)
+            {
+                player.health -= damageDealt;
+                timer = 0;
+            }
         }
     }
 
@@ -34,30 +44,29 @@ public class BrazenBull : MonoBehaviour
     {
         if (collision.CompareTag("Player"))
         {
-            isReady = false;
+            //overlay.SetActive(false);
         }
     }
 
-    public IEnumerator TakeDamage()
-    {
-        while (isReady)
-        {
-            yield return new WaitForSeconds(2.0f);
-            if (!isReady)
-            {
-                yield break;
-            }
-            else
-            {
-                if (!player.playerSlide.isSliding)
-                {
-                    player.health -= damageDealt;
-                }
-                else
-                {
-                    player.health -= damageDealt * 2;
-                }
-            }
-        }
-    }
+    //    public IEnumerator TakeDamage()
+    //    {
+    //        //while (isReady)
+    //        //{
+    //            yield return new WaitForSeconds(2.0f);
+    //            if (!isReady)
+    //            {
+    //                yield break;
+    //            }
+    //            else
+    //            {
+    //                //if (player.playerSlide.isSliding)
+    //                //{
+    //                //    player.health -= damageDealt;
+    //                //}
+    //                //else
+    //                //{
+    //                    player.health -= damageDealt;
+    //                //}
+    //            }
+    //        //}
 }
