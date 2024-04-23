e61897315dddb5481e8ae7036c9a0fac4160405c
MtotMtot <126000669+MtotMtot@users.noreply.github.com>
2024-04-23 20:07:31 +0100

    Artefact Anim partially added
    Artefact idle and collect anim added
    prefab doesnt destroy on collect, need fix. want home

--- Stolen Timelines/Assets/Scripts/Coin.cs
+++ Stolen Timelines/Assets/Scripts/Coin.cs
@@ -8,12 +8,14 @@ public class Coin : MonoBehaviour
     public UnityEvent onCoinCollect;
     public int val;
 
+    private Animator animator;
     private bool collected = false;
-    private void Start()
+    private void Awake()
     {
-        
-       // onCoinCollect.AddListener((int val) => GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>().increaseScore(val));
-      //  onCoinCollect.AddListener(GameObject.FindGameObjectWithTag("UIController").GetComponent<UIController>().updateScore);
+        animator = GameObject.FindWithTag("ArtefactSprite").GetComponent<Animator>();
+        // onCoinCollect.AddListener((int val) => GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>().increaseScore(val));
+        //  onCoinCollect.AddListener(GameObject.FindGameObjectWithTag("UIController").GetComponent<UIController>().updateScore);
+        animator.Play("ArtefactIdle");
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -26,13 +28,20 @@ public class Coin : MonoBehaviour
             GameObject.FindGameObjectWithTag("UIController").GetComponent<UIController>().updateScore();
 
             collected = true;
-           // Debug.Log("Collected");
-            Destroy(gameObject);
+            collectAnim();
+            Debug.Log("Collected");
         }
     }
 
     public void test()
     {
         print("Collected");
+    }
+
+    private IEnumerator collectAnim()
+    {
+        animator.Play("ArtefactCollected");
+        yield return new WaitWhile(() => animator.GetCurrentAnimatorStateInfo(0).normalizedTime <= 1.0f);
+        Destroy(this);
     }
 }
