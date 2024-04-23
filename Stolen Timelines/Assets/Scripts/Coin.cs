ed77ffa8dae950a8ec680d775187a593cf506952
MtotMtot <louisgbaker@hotmail.com>
2024-04-23 23:18:08 +0100

    artefact value changes
    changed artefact vals for varied score (were all 25)

--- Stolen Timelines/Assets/Scripts/Coin.cs
+++ Stolen Timelines/Assets/Scripts/Coin.cs
@@ -8,14 +8,11 @@ public class Coin : MonoBehaviour
     public UnityEvent onCoinCollect;
     public int val;
 
-    private Animator animator;
     private bool collected = false;
     private void Awake()
     {
-        animator = GameObject.FindWithTag("ArtefactSprite").GetComponent<Animator>();
         // onCoinCollect.AddListener((int val) => GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>().increaseScore(val));
         //  onCoinCollect.AddListener(GameObject.FindGameObjectWithTag("UIController").GetComponent<UIController>().updateScore);
-        animator.Play("ArtefactIdle");
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -28,20 +25,12 @@ public class Coin : MonoBehaviour
             GameObject.FindGameObjectWithTag("UIController").GetComponent<UIController>().updateScore();
 
             collected = true;
-            collectAnim();
-            Debug.Log("Collected");
+            Destroy(gameObject);
         }
     }
 
     public void test()
     {
         print("Collected");
-    }
-
-    private IEnumerator collectAnim()
-    {
-        animator.Play("ArtefactCollected");
-        yield return new WaitWhile(() => animator.GetCurrentAnimatorStateInfo(0).normalizedTime <= 1.0f);
-        Destroy(this);
     }
 }
