65292acddc44d5398608caacef45492ef5fa3685
NiallCrammond <2102699@uad.ac.uk>
2024-04-24 15:50:49 +0100

    BUG FIXES

--- Stolen Timelines/Assets/ArtefactSpawner.cs
+++ Stolen Timelines/Assets/ArtefactSpawner.cs
@@ -15,15 +15,11 @@ public class ArtefactSpawner : MonoBehaviour
 
         for (int i = 0; i < spawnCount; i++)
         {
-
             int spawnPointIndex = Random.Range(0, spawnPositions.Count);
             Transform spawnPoint = spawnPositions[spawnPointIndex];
             GameObject A = artefacts[Random.Range(0, artefacts.Count)];
-            Debug.Log("Spawning artefact at: " + new Vector3(spawnPositions[i].position.x, spawnPositions[i].position.y, spawnPositions[i].position.y));
             Instantiate(A, spawnPoint.position, Quaternion.identity);
-
             spawnPositions.Remove(spawnPositions[spawnPointIndex]);
-
         }
 
     }
